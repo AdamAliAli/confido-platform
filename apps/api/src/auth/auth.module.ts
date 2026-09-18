1dd7d315ce7b3af4310b54f723d4fdb1a067df51
AdamAliAli <164984959+AdamAliAli@users.noreply.github.com>
2026-09-18 23:14:37 +0800

    Remove Redis from auth module

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -1,1 +1,16 @@
-import { Module } from '@nestjs/common'; import { JwtModule } from '@nestjs/jwt'; import { PassportModule } from '@nestjs/passport'; import { PrismaService } from '../common/prisma.service'; import { RedisService } from '../common/redis.service'; import { AuthController } from './auth.controller'; import { AuthService } from './auth.service'; import { JwtStrategy } from './jwt.strategy'; import { RolesGuard } from './roles.guard'; @Module({imports:[PassportModule,JwtModule.register({})],controllers:[AuthController],providers:[AuthService,JwtStrategy,RolesGuard,PrismaService,RedisService],exports:[RolesGuard]})export class AuthModule{}
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { PrismaService } from '../common/prisma.service';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
+import { RolesGuard } from './roles.guard';
+
+@Module({
+  imports: [PassportModule, JwtModule.register({})],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy, RolesGuard, PrismaService],
+  exports: [RolesGuard],
+})
+export class AuthModule {}
